c5d04c06ce0c7717089f590a2780a83d66d1b3a8
Wei-Zhong Tan <wztan@hotmail.co.uk>
2017-02-03 17:26:09 +0000

    Changed error messages

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const crypto = require('crypto')
 
 const app = express()
 const port = process.env.PORT || 8000
-const mongoUrl = process.env.MONGODB_URI || `mongodb://${ip}/local`
+const idLen = 32 // hash length for hex MD5 hash
 
-// apply body parser middleware to post requests
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+// set mongodb url for heroku/c9.io running environments
+const mongoUrl = process.env.MONGODB_URI || `mongodb://${process.env.IP}/local` || null
+
+// parse application/x-www-form-urlencoded requests
+app.use(bodyParser.urlencoded( { extended: true } ))
 
 // router for string creation
 app.post('/messages/', (req, res) => {
@@ -19,11 +19,11 @@ app.post('/messages/', (req, res) => {
   const text = Object.getOwnPropertyNames(req.body)[0]
 
   // reject incredibly long strings
-  if (text.length > 1000) {
-    res.end('String must be less than 1000 characters long.')
+  if (text.length > 1000000) {
+    res.end('Messages must be <1MB.\n')
   } else {
     // compute hash of text string
-    const hash = crypto.createHash('md5').update(text).digest('hex');
+    const hash = crypto.createHash('md5').update(text).digest('hex')
 
     // open database
     MongoClient.connect(mongoUrl, (err, db) => {
@@ -35,19 +35,20 @@ app.post('/messages/', (req, res) => {
       // define text document
       const doc = { _id: hash, text }
 
+      // insert text doc into strings collection
       strings.insert(doc, (err, data) => {
         if (err) {
           // catch errors due to index duplication
           if (err.code === 11000) {
             // return duplicate id to user
-            res.end(JSON.stringify( { id: hash } ))
+            res.end(`${JSON.stringify( { id: hash } )}\n`)
           } else {
-            // otherwise throw other errors
+            // throw other errors
             throw err
           }
         } else {
           // return id to user
-          res.end(JSON.stringify({ id: data.ops[0]._id }))
+          res.end(`${JSON.stringify( { id: data.ops[0]._id } )}\n`)
         }
 
         // close database
@@ -63,42 +64,42 @@ app.get('/messages/:id', (req, res) => {
   const id = req.params.id
 
   // validate id length
-  if (id.length > 0) {
+  if (id.length !== idLen) {
+    // reject incorrect length hashes
+    res.end(`Id length should be ${idLen}.\n`)
+  } else {
     // open database
     MongoClient.connect(mongoUrl, (err, db) => {
       if (err) throw err
 
       // open collection
-      const strings = db.collection("strings")
+      const strings = db.collection('strings')
 
       // search for entry by index (binary tree search is very fast)
       strings.find( { _id: id } )
       // convert cursor to array
       .toArray((err, docs) => {
-        const doc = docs[0]
+        if (err) throw err
 
         // send text as response
+        const doc = docs[0]
         if (doc) {
-          res.end(doc.text)
+          res.end(`${doc.text}\n`)
         } else {
-          // reject id
-          res.end('Invalid id.')
+          // reject id if not found in collection
+          res.end('Id not found.\n')
         }
       })
 
       // close database
       db.close()
     })
-
-  } else {
-    // reject id
-    res.end(`Invalid id.`)
   }
 })
 
 // default response
 app.use('/*', (req, res) => {
-  res.end('Usage: $domain/messages/ -d "message"\nor:    $domain/messages/id')
+  res.end('Usage: curl $domain/messages/ -d "message"\nor:    curl $domain/messages/id\n')
 })
 
 app.listen(port, () => {
